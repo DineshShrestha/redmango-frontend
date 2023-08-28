10605b18371a9f0ca7458ae7c1e34a846fdd1ddd
sdin <sdin@LAPTOP-DINESH>
2023-08-28 11:15:54 +0200

    Card Component

---
+++ src/Components/Page/MenuItems/MenuItemCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { menuItemModel } from '../../../Interfaces'
+
+interface Props {
+    menuItem: menuItemModel
+}
+function MenuItemCard(props: Props) {
+  return (
+    <div>{props.menuItem.name}</div>
+  )
+}
+
+export default MenuItemCard
