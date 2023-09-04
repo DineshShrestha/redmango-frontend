cc8f4106f8a8487ae4996fac4f9a9c04100588b5
sdin <sdin@LAPTOP-DINESH>
2023-09-04 12:16:55 +0200

    shopping cart functional

--- src/Components/Cart/CartSummary.tsx
+++ src/Components/Cart/CartSummary.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { cartItemModel } from '../../Interfaces';
 import { RootState } from '../../Storage/Redux/store';
+import { removeFromCart, updateQuantity } from '../../Storage/Redux/shoppingCartSlice';
 function CartSummary() {
-
+    const dispatch = useDispatch();
     const shoppingCartFromStore : cartItemModel[] = useSelector(
         (state: RootState)=>state.shoppingCartStore.cartItems??[]
     )
     if(!shoppingCartFromStore){
         return <div>Shopping Cart Empty</div>
+    }
+
+    const handleQuantity =(updateQuantityBy:number, cartItem: cartItemModel)=>{
+      if((updateQuantityBy == -1 && cartItem.quantity == 1)|| updateQuantityBy==0){
+        //remove the item
+        dispatch(removeFromCart({cartItem, quantity:0}))
+      }
+      else{
+        //update the quantity with the new quantity
+        dispatch(updateQuantity({cartItem, quantity:cartItem.quantity!+updateQuantityBy}));
+      }
     }
   return (
     <div className="container p-4 m-2">
@@ -46,17 +58,17 @@ function CartSummary() {
                }}
              >
                <span style={{ color: "rgba(22,22,22,.7)" }} role="button">
-                 <i className="bi bi-dash-circle-fill"></i>
+                 <i className="bi bi-dash-circle-fill" onClick={()=>handleQuantity(-1, cartItem)}></i>
                </span>
                <span>
                  <b>{cartItem.quantity}</b>
                </span>
                <span style={{ color: "rgba(22,22,22,.7)" }} role="button">
-                 <i className="bi bi-plus-circle-fill"></i>
+                 <i className="bi bi-plus-circle-fill" onClick={()=>handleQuantity(+1, cartItem)}></i>
                </span>
              </div>
    
-             <button className="btn btn-danger mx-1">Remove</button>
+             <button className="btn btn-danger mx-1" onClick={()=>handleQuantity(0, cartItem)}>Remove</button>
            </div>
          </div>
        </div>
