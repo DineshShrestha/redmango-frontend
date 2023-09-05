e707d70f0345a04c60c4ea48a828c8ff206a6f0b
sdin <sdin@LAPTOP-DINESH>
2023-09-05 16:08:04 +0200

    Response API interface

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -2,4 +2,5 @@ import menuItemModel from "./menuItemModel";
 import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
-export type {menuItemModel, shoppingCartModel, cartItemModel, userModel};
+import apiResponse from "./apiResponse";
+export type {menuItemModel, shoppingCartModel, cartItemModel, userModel, apiResponse};
