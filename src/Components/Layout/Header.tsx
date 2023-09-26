2ba6cc691d63e8394044bc7e7b6876e24055245b
sdin <sdin@LAPTOP-DINESH>
2023-09-26 16:06:17 +0200

    cleanup test components

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -38,16 +38,7 @@ const handleLogout =()=>{
               {userData.id && `(${shoppingCartFromStore.length})`}
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link " aria-current="page" to="/authentication">
-              Authentication
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link " aria-current="page" to="/authorization">
-              Authorization
-            </NavLink>
-          </li>
+         
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Admin panel
