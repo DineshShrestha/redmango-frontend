2627f4f2b65f50e17da9101205be54580f42d932
sdin <sdin@LAPTOP-DINESH>
2023-10-13 16:23:59 +0200

    dynamic my orders

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -3,4 +3,6 @@ import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
 import apiResponse from "./apiResponse";
-export type {menuItemModel, shoppingCartModel, cartItemModel, userModel, apiResponse};
+import orderHeader from "./orderHeader";
+import orderDetail from "./orderDetail";
+export type {orderDetail, orderHeader, menuItemModel, shoppingCartModel, cartItemModel, userModel, apiResponse};
