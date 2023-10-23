25eadc33464e6a9d83e545c49391fe0d984963f0
sdin <sdin@LAPTOP-DINESH>
2023-10-23 16:54:12 +0200

    delete menu item

--- src/Pages/MenuItem/MenuItemList.tsx
+++ src/Pages/MenuItem/MenuItemList.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { useGetMenuItemsQuery } from '../../Apis/menuItemApi'
+import { useDeleteMenuItemMutation, useGetMenuItemsQuery } from '../../Apis/menuItemApi'
 import { MainLoader } from '../../Components/Page/Common';
 import { menuItemModel } from '../../Interfaces';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 function MenuItemList() {
+    const [deleteMenuItem] = useDeleteMenuItemMutation();
     const { data, isLoading } = useGetMenuItemsQuery(null);
     const navigate = useNavigate();
+    const handleMenuItemDelete = async (id: number)=>{
+        
+        toast.promise(
+            deleteMenuItem(id),
+            {
+                pending: 'Processing your request .....',
+                success: 'Menu Item Deleted Successfully',
+                error: 'Error encountered'
+            },
+            {
+                theme: "dark"
+            }
+        )
+    }
     return (
         <>
             {isLoading && <MainLoader />}
@@ -44,7 +60,7 @@ function MenuItemList() {
                                         <button className="btn btn-success" >
                                             <i className="bi bi-pencil-fill" onClick={()=>navigate("/menuitem/menuitemupsert/"+menuItem.id)}></i>
                                         </button>
-                                        <button className="btn btn-danger mx-2" >
+                                        <button className="btn btn-danger mx-2" onClick={()=> handleMenuItemDelete(menuItem.id)}>
                                             <i className="bi bi-trash-fill" ></i>
                                         </button>
                                     </div>
