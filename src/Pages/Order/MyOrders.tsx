cfbea4ae052e6d6476009fe2f85813f6d28b8695
sdin <sdin@LAPTOP-DINESH>
2023-10-25 10:02:33 +0200

    Add UI Filters

--- src/Pages/Order/MyOrders.tsx
+++ src/Pages/Order/MyOrders.tsx
@@ -5,7 +5,15 @@ import { RootState } from '../../Storage/Redux/store';
 import { useGetAllOrdersQuery } from '../../Apis/orderApi';
 import OrderList from './OrderList';
 import { MainLoader } from '../../Components/Page/Common';
+import { SD_Status } from '../../Utility/SD';
 
+const filterOptions = [
+  "ALL",
+  SD_Status.CONFIRMED,
+  SD_Status.BEING_COOKED,
+  SD_Status.READY_FOR_PICKUP,
+  SD_Status.CANCELLED,
+];
 function MyOrders() {
     const userId = useSelector((state: RootState)=> state.userAuthStore.id);
     const {data, isLoading} = useGetAllOrdersQuery(userId);
@@ -13,7 +21,12 @@ function MyOrders() {
     <>
     {isLoading && <MainLoader/>}
     {!isLoading && (
+      <>
+      <div className='d-flex align-items-center justify-content-between mx-5 mt-5'>
+        <h1 className="text-success">My Orders</h1>
+      </div>
     <OrderList isLoading={isLoading} orderData={data.result}/>
+    </>
     )}
     </>
   )
